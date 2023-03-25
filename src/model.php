223e6b41b176b782dd72df9a5133aa8604072c5f
91ahmed <ahmedh12491@gmail.com>
2023-03-25 14:39:28 +0200

    new update

---
+++ src/model.php
@@ -0,0 +1,26 @@
+<?php
+	
+	namespace PhpSqlBuilder;
+
+	use PhpSqlBuilder\SQL;
+	
+	class Model extends SQL
+	{
+		public static $connect = [
+			'driver' => 'mysql',
+			'host' => '127.0.0.1',
+			'database' => 'test',
+			'user' => 'root',
+			'password' => '',
+			'port' => 3306,
+			'charset' => 'utf8',
+		];
+
+		public static function query ()
+		{
+			$query = new SQL(static::$connect);
+
+			return $query->table(static::$table);
+		}
+	}
+?>
